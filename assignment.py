07dadb7312dbd31e9f5e581af2edf455ce650fbd
Sam Luby <samwluby@gmail.com>
2018-03-03 16:46:40 +0000

    CSV writing to file formatting working

--- assignment.py
+++ assignment.py
@@ -15,12 +15,14 @@ import csv
 baseurl = "https://query.yahooapis.com/v1/public/yql?"
 client = yweather.Client()
 
+
 locations = ["Dublin, Ireland", "London, England", "Paris, France", "Berlin, Germany", "Stockholm, Sweden", "Moscow, Russia", "Tokyo, Japan", "Sydney, Australia", "New York City, United States", "Los Angeles, United States"]
 
 weather_headings = ["country", "city", "data", "highTemp", "lowTemp"]
 locations_woeid = []
 
 data_file = "weather_data.csv"
+forecast_days = 10
 
 
 def create_file(file_location, date_headings):
@@ -32,9 +34,9 @@ def create_file(file_location, date_headings):
 
 def write_to_file(file_location, data):
     with open(file_location, 'a') as write_file:
-        f = csv.writer(write_file, delimiter=",", dialect='excel')
+        f = csv.writer(write_file, delimiter=",", dialect='excel', lineterminator = '\n')
         f.writerow(data)
-    write_file.close()
+    # write_file.close()
 
 
 # get woeid (where on earth ID) from location names
@@ -49,7 +51,6 @@ def translate_woeids(locations):
 
 def get_data(locations):
     for i in range(len(locations)):
-        data = []
         print(i)
         x = locations_woeid[i]
         yql_query = "select location, item.forecast from weather.forecast where woeid=" + x + " and u = 'C'"
@@ -57,28 +58,23 @@ def get_data(locations):
         result = urllib.request.urlopen(yql_url).read()
         parsed_data = json.loads(result)
 
-        city = parsed_data['query']['results']['channel'][0]['location']['city']
-        data.append(str(city))
-
-        country = parsed_data['query']['results']['channel'][0]['location']['country']
-        data.append(str(country))
-
-        date = parsed_data['query']['results']['channel'][0]['item']['forecast']['date']
-        data.append(str(date))
-
-        high_temp = (parsed_data['query']['results']['channel'][0]['item']['forecast']['high'])
-        data.append(str(high_temp))
-
-        low_temp = (parsed_data['query']['results']['channel'][0]['item']['forecast']['low'])
-        data.append(str(low_temp))
-    return data
+        for x in range(forecast_days):
+            data = []
+            data.append(str(parsed_data['query']['results']['channel'][x]['location']['city']))
+            data.append(str(parsed_data['query']['results']['channel'][x]['location']['country']))
+            data.append(str(parsed_data['query']['results']['channel'][x]['item']['forecast']['date']))
+            data.append(str(parsed_data['query']['results']['channel'][x]['item']['forecast']['high']))
+            data.append(str(parsed_data['query']['results']['channel'][x]['item']['forecast']['low']))
+            write_to_file(data_file, data)
+        write_to_file(data_file, data)
 
 
-translate_woeids(locations)
+
 write_to_file(data_file, weather_headings)
-data = get_data(locations)
-write_to_file(data_file, data)
+translate_woeids(locations)
+get_data(locations)
 
-print(data)
+
+# print(data)
 
 # print(parsed_data['query']['results']['channel'][0]['location']['city'], parsed_data['query']['results']['channel'][0]['location']['country'], parsed_data['query']['results']['channel'][0]['item']['forecast']['date'], parsed_data['query']['results']['channel'][0]['item']['forecast']['high'], parsed_data['query']['results']['channel'][0]['item']['forecast']['low'])
